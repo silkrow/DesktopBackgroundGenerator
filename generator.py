bf89b90f3851760d73a037e20a73ffc3549ab658
silkrow <si1krow@outlook.com>
2022-10-20 17:39:25 -0500

    working version

--- generator.py
+++ generator.py
@@ -1,24 +1,27 @@
 from PIL import Image, ImageDraw, ImageFont
-from random import randint
+from random import sample
 
 # general settings
 width = 1920
 height = 1080
-font = ImageFont.truetype("Monaco.ttf", size = 20)
+num_pics = 10
+font = ImageFont.truetype("Monaco.ttf", size = 17)
 
 # grab random QA
-with open('QA.md') as f:
+with open('texts/QA.md') as f:
 	contents = f.read()
 	qa_texts = contents.split("---")
 
-qa_index = randint(0, len(qa_texts) - 1)
 
 # generate picture
-message = qa_texts[qa_index]
-img = Image.new('RGB', (width, height), color = 'blue')
+qa_indices = sample(range(len(qa_texts)), num_pics)
+for i in range(num_pics):
+	message = qa_texts[qa_indices[i]]
+	img = Image.new('RGB', (width, height), color = 'black')
 
-imgDraw = ImageDraw.Draw(img)
+	imgDraw = ImageDraw.Draw(img)
 
-imgDraw.text((10, 10), message, font = font,  fill=(255, 255, 0))
+	imgDraw.text((50, 100), message, font = font,  fill=(255, 255, 255))
 
-img.save('pic/1.png')
+	imgName = 'pic/'+ str(i) + '.png'
+	img.save(imgName)

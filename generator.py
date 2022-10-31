7ccb678ef0a16e63c81c9b8a866af4b85babd14a
silkrow <si1krow@outlook.com>
2022-10-30 23:23:53 -0500

    update codes

--- generator.py
+++ generator.py
@@ -5,6 +5,7 @@ from random import sample
 width = 1920
 height = 1080
 num_pics = 10
+num_vocs = 10
 qa_x = 100
 qa_y = 50
 voc_x = 1100
@@ -31,15 +32,14 @@ greet1 = "Please please get things done EARLY!"
 
 # generate picture
 qa_indices = sample(range(len(qa_texts)), num_pics)
-voc_indices = sample(range(len(voc_texts)), len(voc_texts))
+voc_indices = sample(range(len(voc_texts)), num_vocs)
 for i in range(num_pics):
-	qa_texts[qa_indices[i]]
 	img = Image.new('RGB', (width, height), color = (238,232,170))
 
 	imgDraw = ImageDraw.Draw(img)
 
 	imgDraw.text((qa_x, qa_y), qa_texts[qa_indices[i]], font = qa_font,  fill=(0, 0, 0))
-	imgDraw.text((voc_x, voc_y), voc_texts[voc_indices[1]], font = voc_font,  fill=(0, 0, 0))
+	imgDraw.text((voc_x, voc_y), voc_texts[voc_indices[i]], font = voc_font,  fill=(0, 0, 0))
 	imgDraw.text((greet1_x, greet1_y), greet1, font = greet1_font,  fill=(0, 0, 0))
 	imgName = 'pic/'+ str(i) + '.png'
 	img.save(imgName)
